589e000b0838f81c304284f417c7e9578bec715a
MarzukaaZaki <zakimarzuka@gmail.com>
2023-06-12 09:04:33 +0600

    Added confirm password function

--- src/layout/Register.jsx
+++ src/layout/Register.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 const Register = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const validationSchema = Yup.object().shape({
+        password: Yup.string()
+            .required('Password is required')
+            .min(6, 'Password must be at least 6 characters'),
+        confirmPassword: Yup.string()
+            .required('Confirm Password is required')
+            .oneOf([Yup.ref('password')], 'Passwords must match')
+            
+    });
+    const formOptions = { resolver: yupResolver(validationSchema) };
+
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, reset, formState } = useForm(formOptions);
+    const { errors } = formState;
+
+    function onSubmit(data) {
+        console.log(data);
+    }
+
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col">
@@ -12,7 +31,7 @@ const Register = () => {
 
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-200 border border-t-0">
-                    <div className="card-body" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="card-body">
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-control">
                             <label className="label">
@@ -36,18 +55,26 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" {...register("password", { required: true })} className="input input-bordered" />
+                            <input type='password' placeholder='password' {...register("password")} className="input input-bordered" />
                             {/* errors will return when field validation fails  */}
-                            {errors.password && <span className='text-red-500 text-sm'>Password field is required!</span>}
-                            
+                            {errors.password && <span className='text-red-500 text-sm'>{errors.password?.message}</span>}
                         </div>
+                       
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Confirm Password</span>
                             </label>
-                            <input type="password" placeholder="confirm password" {...register("confirmPassword", { required: true }, {validate: (value) => value === watch('password')})} className="input input-bordered" />
+                            <input type="password" placeholder="confirm password" {...register("confirmPassword")} className="input input-bordered" />
                             {/* errors will return when field validation fails  */}
-                            {errors.confirmPassword && <span className='text-red-500 text-sm'>Passwords must match!</span>}
+                            {errors.confirmPassword && <span className='text-red-500 text-sm'>{errors.confirmPassword?.message}</span>}
+                            
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo</span>
+                            </label>
+                            <input type="file" placeholder="any image format" {...register("photo", {required: true})} accept='photo/*' className="input input-bordered" />
+                            {/* errors will return when field validation fails  */}
                             
                         </div>
                       
